3f068f2bfe14d6c0f54f64c8cfa414f37972c2eb
codeher-rahavi <rahaviganeshan529@gmail.com>
2024-11-26 15:47:53 +0530

    c problem sheets

---
+++ shifting of numbers/main.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    int num,i,shift=0;
+    scanf("%d",&num);
+    int arr[num];
+    for(i=0;i<num;i++)
+    {
+        scanf("%d",&arr[i]);
+    }
+    scanf("%d",&shift);
+    shift=shift%num;
+    int temp[num];
+    for(i=0;i<num;i++)
+    {
+       temp[(i+shift)%num]=arr[i];
+
+    }
+    for(i=0;i<num;i++)
+    {
+        arr[i]=temp[i];
+    }
+    for(i=0;i<num;i++)
+    {
+        printf("%d ",arr[i]);
+    }
+    return 0;
+}
