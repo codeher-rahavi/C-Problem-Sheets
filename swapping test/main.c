32325a84c3cb77244d5e39b2e3a2fe52eebdbd5e
codeher-rahavi <rahaviganeshan529@gmail.com>
2024-11-28 12:11:02 +0530

    c problem sheets

---
+++ swapping test/main.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    int num,temp=0;
+    scanf("%d",&num);
+    int i,arr[num];
+    for(i=0;i<num;i++)
+    {
+        scanf("%d",&arr[i]);
+    }
+    for(i=0;i<num/2;i++)
+    {
+        temp=arr[i];
+        arr[i]=arr[num-i-1];
+        arr[num-i-1]=temp;
+    }
+    for(i=0;i<num;i++)
+    {
+        printf("%d ",arr[i]);
+    }
+    return 0;
+}
