bf2fdbd1d555c4ba75d1e644a96c8673f68ad868
codeher-rahavi <rahaviganeshan529@gmail.com>
2025-03-09 19:21:39 +0530

    c problem sheets

---
+++ matrix 90 degree rotatiom/main.c
@@ -0,0 +1,51 @@
+#include <stdio.h>
+#include <string.h>
+
+int main()
+{
+    int num;
+    scanf("%d",&num);
+    char a[num][num];
+    int i,j,k;
+    for(i=0;i<num;i++)
+    {
+        for(j=0;j<num;j++)
+        {
+            scanf(" %c", &a[i][j]);
+        }
+    }
+
+    //transpose
+
+    for(i=0;i<num;i++)
+    {
+        for(j=i;j<num;j++)
+        {
+            char temp = a[i][j];
+            a[i][j] = a[j][i];
+            a[j][i] = temp;
+        }
+    }
+
+    // rotation
+
+    for(i=0;i<num;i++)
+    {
+        for(j=0,k=num-1;j<k;j++,k--)
+        {
+            char temp = a[i][j];
+            a[i][j] = a[i][k];
+            a[i][k] = temp;
+        }
+    }
+
+    for(i=0;i<num;i++)
+    {
+        for(j=0;j<num;j++)
+        {
+            printf("%c ",a[i][j]);
+        }
+        printf("\n");
+    }
+    return 0;
+}
