fdb000b9745574e5096731361fab428510271ed3
codeher-rahavi <rahaviganeshan529@gmail.com>
2025-03-09 19:34:01 +0530

    c problem sheets

---
+++ index of the given word/main.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <string.h>
+
+int main()
+{
+    char name[50];
+    fgets(name,sizeof(name),stdin);
+    name[strcspn(name,"\n")]='\0';
+
+    char find[10];
+    scanf("%s",&find);
+
+    int i;
+    for(i=0;name[i]!='\0';i++)
+    {
+        if(name[i]==find[0])
+        {
+            printf("%d",i);
+            break;
+        }
+    }
+    return 0;
+}
